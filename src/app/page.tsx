8ebd35a984e9bcd6b8e6591cb5a22097e0cdb5cf
Vitor de Paula <vitordepaula98@hotmail.com>
2025-05-26 09:48:26 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<h1 className="font-bold text-6xl tracking-tight">Ola mundo</h1>
+		</div>
+	);
+}
